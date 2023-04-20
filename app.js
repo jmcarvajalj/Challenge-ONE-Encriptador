f6e5ecc05d1aa4c45651be89b55aaee82617786f
josemiguelcarvajal07 <62575167+josemiguelcarvajal07@users.noreply.github.com>
2023-04-19 19:18:09 -0500

    added all functionality

--- app.js
+++ app.js
@@ -1,25 +1,56 @@
-function encriptar() {
-  let textArea = document.getElementById("input-text");
-  let plainText = textArea.value;
-  let encryptedText = plainText
-    .replace(/e/g, "enter")
-    .replace(/i/g, "imes")
-    .replace(/a/g, "ai")
-    .replace(/o/g, "ober")
-    .replace(/u/g, "ufat");
-    
-  console.log(encryptedText);
+function encrypt() {
+    let textArea = document.getElementById("input-text");
+    let plainText = textArea.value;
+    let encryptedText = plainText
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+
+    displayButton();
+    displayTextArea(encryptedText);
 }
 
-function desencriptar() {
-  let textArea = document.getElementById("input-text");
-  let encryptedText = textArea.value;
-  let plainText = encryptedText
-    .replace(/enter/g, "e")
-    .replace(/imes/g, "i")
-    .replace(/ai/g, "a")
-    .replace(/ober/g, "o")
-    .replace(/ufat/g, "u")
+function decrypt() {
+    let textArea = document.getElementById("input-text");
+    let encryptedText = textArea.value;
+    let plainText = encryptedText
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
 
-  console.log(plainText);
+    displayButton();
+    displayTextArea(plainText);
+}
+
+function displayButton() {
+    let parentElement = document.getElementById("result");
+    parentElement.innerHTML = "";
+
+    let button = document.createElement("button");
+    button.innerHTML = "Copiar";
+    button.type = "submit";
+    button.className = "copiar";
+    button.onclick = function () {
+        copyToClipboard();
+    };
+    document.body.appendChild(button);
+}
+
+function displayTextArea(text) {
+    let textArea = document.createElement("textArea");
+    textArea.innerHTML = text;
+    textArea.id = "output-text";
+    textArea.name = "output-text";
+    textArea.setAttribute('readonly', true);
+    document.body.appendChild(textArea);
+}
+
+function copyToClipboard() {
+    let copyText = document.getElementById("output-text");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
 }
